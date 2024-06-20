3ab818af184a33f40033e1fdc9d5cac6569c924f
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:20:52 +0300

    SimpleScript.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/SimpleScript.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/SimpleScript.cs
@@ -1,21 +1,26 @@
+using TMPro;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class SimpleScript : MonoBehaviour
 {
-	public class SimpleScript : MonoBehaviour
+	private TextMeshPro m_textMeshPro;
+
+	private const string label = "The <#0050FF>count is: </color>{0:2}";
+
+	private float m_frame;
+
+	private void Start()
 	{
-		private TextMeshPro m_textMeshPro;
+		m_textMeshPro = base.gameObject.AddComponent<TextMeshPro>();
+		m_textMeshPro.autoSizeTextContainer = true;
+		m_textMeshPro.fontSize = 48f;
+		m_textMeshPro.alignment = TextAlignmentOptions.Center;
+		m_textMeshPro.enableWordWrapping = false;
+	}
 
-		private const string label = "The <#0050FF>count is: </color>{0:2}";
-
-		private float m_frame;
-
-		private void Start()
-		{
-		}
-
-		private void Update()
-		{
-		}
+	private void Update()
+	{
+		m_textMeshPro.SetText("The <#0050FF>count is: </color>{0:2}", m_frame % 1000f);
+		m_frame += 1f * Time.deltaTime;
 	}
 }
