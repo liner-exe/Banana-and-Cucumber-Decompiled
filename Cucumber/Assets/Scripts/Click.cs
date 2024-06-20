a6e1a7d7fffd5e857d0b04d58e9c1e909bcaa3de
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:34:52 +0300

    Click.cs

--- Cucumber/Assets/Scripts/Click.cs
+++ Cucumber/Assets/Scripts/Click.cs
@@ -1,3 +1,4 @@
+using Steamworks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -10,13 +11,32 @@ public class Click : MonoBehaviour, IPointerClickHandler, IEventSystemHandler
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
+		if (eventData.pointerCurrentRaycast.gameObject == base.gameObject)
+		{
+			count++;
+			text.text = count.ToString();
+		}
 	}
 
 	private void Start()
 	{
+		if (!PlayerPrefs.HasKey("FirstStart"))
+		{
+			PlayerPrefs.SetInt("FirstStart", 1);
+			SteamUserStats.SetAchievement("NEW_ACHIEVEMENT_1");
+			SteamUserStats.StoreStats();
+		}
+		if (!PlayerPrefs.HasKey("FirstStart2"))
+		{
+			PlayerPrefs.SetInt("FirstStart2", 1);
+			SteamUserStats.SetAchievement("NEW_ACHIEVEMENT_1_1");
+			SteamUserStats.StoreStats();
+		}
 	}
 
 	public void ResetAchievement()
 	{
+		SteamUserStats.ClearAchievement("NEW_ACHIEVEMENT_1");
+		SteamUserStats.StoreStats();
 	}
 }
