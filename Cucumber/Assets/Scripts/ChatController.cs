f06d74778860c68586a996d494b22f61b2e9e99d
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:34:23 +0300

    ChatController.cs

--- Cucumber/Assets/Scripts/ChatController.cs
+++ Cucumber/Assets/Scripts/ChatController.cs
@@ -1,5 +1,7 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ChatController : MonoBehaviour
@@ -12,13 +14,32 @@ public class ChatController : MonoBehaviour
 
 	private void OnEnable()
 	{
+		((UnityEvent<string>)(object)ChatInputField.onSubmit).AddListener((UnityAction<string>)AddToChatOutput);
 	}
 
 	private void OnDisable()
 	{
+		((UnityEvent<string>)(object)ChatInputField.onSubmit).RemoveListener((UnityAction<string>)AddToChatOutput);
 	}
 
 	private void AddToChatOutput(string newText)
 	{
+		ChatInputField.text = string.Empty;
+		DateTime now = DateTime.Now;
+		string text = "[<#FFFF80>" + now.Hour.ToString("d2") + ":" + now.Minute.ToString("d2") + ":" + now.Second.ToString("d2") + "</color>] " + newText;
+		if (ChatDisplayOutput != null)
+		{
+			if (ChatDisplayOutput.text == string.Empty)
+			{
+				ChatDisplayOutput.text = text;
+			}
+			else
+			{
+				TMP_Text chatDisplayOutput = ChatDisplayOutput;
+				chatDisplayOutput.text = chatDisplayOutput.text + "\n" + text;
+			}
+		}
+		ChatInputField.ActivateInputField();
+		ChatScrollbar.value = 0f;
 	}
 }
