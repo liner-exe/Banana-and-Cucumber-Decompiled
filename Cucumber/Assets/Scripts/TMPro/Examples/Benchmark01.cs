6befd576783342d895f3216278422187b99cd3ba
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:16:15 +0300

    Benchmark01.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/Benchmark01.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/Benchmark01.cs
@@ -1,88 +1,90 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class Benchmark01 : MonoBehaviour
 {
-	public class Benchmark01 : MonoBehaviour
+	public int BenchmarkType;
+
+	public TMP_FontAsset TMProFont;
+
+	public Font TextMeshFont;
+
+	private TextMeshPro m_textMeshPro;
+
+	private TextContainer m_textContainer;
+
+	private TextMesh m_textMesh;
+
+	private const string label01 = "The <#0050FF>count is: </color>{0}";
+
+	private const string label02 = "The <color=#0050FF>count is: </color>";
+
+	private Material m_material01;
+
+	private Material m_material02;
+
+	private IEnumerator Start()
 	{
-		[CompilerGenerated]
-		private sealed class _003CStart_003Ed__10 : IEnumerator<object>, IEnumerator, IDisposable
+		if (BenchmarkType == 0)
 		{
-			private int _003C_003E1__state;
-
-			private object _003C_003E2__current;
-
-			public Benchmark01 _003C_003E4__this;
-
-			private int _003Ci_003E5__2;
-
-			private object System_002ECollections_002EGeneric_002EIEnumerator_003CSystem_002EObject_003E_002ECurrent
+			m_textMeshPro = base.gameObject.AddComponent<TextMeshPro>();
+			m_textMeshPro.autoSizeTextContainer = true;
+			if (TMProFont != null)
 			{
-				[DebuggerHidden]
-				get
+				m_textMeshPro.font = TMProFont;
+			}
+			m_textMeshPro.fontSize = 48f;
+			m_textMeshPro.alignment = TextAlignmentOptions.Center;
+			m_textMeshPro.extraPadding = true;
+			m_textMeshPro.enableWordWrapping = false;
+			m_material01 = m_textMeshPro.font.material;
+			m_material02 = Resources.Load<Material>("Fonts & Materials/LiberationSans SDF - Drop Shadow");
+		}
+		else if (BenchmarkType == 1)
+		{
+			m_textMesh = base.gameObject.AddComponent<TextMesh>();
+			if (TextMeshFont != null)
+			{
+				m_textMesh.font = TextMeshFont;
+				m_textMesh.GetComponent<Renderer>().sharedMaterial = m_textMesh.font.material;
+			}
+			else
+			{
+				m_textMesh.font = Resources.Load("Fonts/ARIAL", typeof(Font)) as Font;
+				m_textMesh.GetComponent<Renderer>().sharedMaterial = m_textMesh.font.material;
+			}
+			m_textMesh.fontSize = 48;
+			m_textMesh.anchor = TextAnchor.MiddleCenter;
+		}
+		for (int i = 0; i <= 1000000; i++)
+		{
+			if (BenchmarkType == 0)
+			{
+				m_textMeshPro.SetText("The <#0050FF>count is: </color>{0}", i % 1000);
+				if (i % 1000 == 999)
 				{
-					return null;
+					TextMeshPro textMeshPro = m_textMeshPro;
+					Material fontSharedMaterial;
+					if (!(m_textMeshPro.fontSharedMaterial == m_material01))
+					{
+						Material material2 = (m_textMeshPro.fontSharedMaterial = m_material01);
+						fontSharedMaterial = material2;
+					}
+					else
+					{
+						Material material2 = (m_textMeshPro.fontSharedMaterial = m_material02);
+						fontSharedMaterial = material2;
+					}
+					textMeshPro.fontSharedMaterial = fontSharedMaterial;
 				}
 			}
-
-			private object System_002ECollections_002EIEnumerator_002ECurrent
+			else if (BenchmarkType == 1)
 			{
-				[DebuggerHidden]
-				get
-				{
-					return null;
-				}
+				m_textMesh.text = "The <color=#0050FF>count is: </color>" + i % 1000;
 			}
-
-			[DebuggerHidden]
-			public _003CStart_003Ed__10(int _003C_003E1__state)
-			{
-			}
-
-			[DebuggerHidden]
-			private void System_002EIDisposable_002EDispose()
-			{
-			}
-
-			private bool MoveNext()
-			{
-				return false;
-			}
-
-			[DebuggerHidden]
-			private void System_002ECollections_002EIEnumerator_002EReset()
-			{
-			}
+			yield return null;
 		}
-
-		public int BenchmarkType;
-
-		public TMP_FontAsset TMProFont;
-
-		public Font TextMeshFont;
-
-		private TextMeshPro m_textMeshPro;
-
-		private TextContainer m_textContainer;
-
-		private TextMesh m_textMesh;
-
-		private const string label01 = "The <#0050FF>count is: </color>{0}";
-
-		private const string label02 = "The <color=#0050FF>count is: </color>";
-
-		private Material m_material01;
-
-		private Material m_material02;
-
-		[IteratorStateMachine(typeof(_003CStart_003Ed__10))]
-		private IEnumerator Start()
-		{
-			return null;
-		}
+		yield return null;
 	}
 }
