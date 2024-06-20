05077cb83ec0e32efe214a8b69d51fea3ef1bff5
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:30:15 +0300

    VertexJitter.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/VertexJitter.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/VertexJitter.cs
@@ -1,113 +1,118 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using TMPro;
+using TMPro.Examples;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class VertexJitter : MonoBehaviour
 {
-	public class VertexJitter : MonoBehaviour
+	private struct VertexAnim
 	{
-		private struct VertexAnim
+		public float angleRange;
+
+		public float angle;
+
+		public float speed;
+	}
+
+	public float AngleMultiplier = 1f;
+
+	public float SpeedMultiplier = 1f;
+
+	public float CurveScale = 1f;
+
+	private TMP_Text m_TextComponent;
+
+	private bool hasTextChanged;
+
+	private void Awake()
+	{
+		m_TextComponent = GetComponent<TMP_Text>();
+	}
+
+	private void OnEnable()
+	{
+		TMPro_EventManager.TEXT_CHANGED_EVENT.Add((Action<UnityEngine.Object>)ON_TEXT_CHANGED);
+	}
+
+	private void OnDisable()
+	{
+		TMPro_EventManager.TEXT_CHANGED_EVENT.Remove((Action<UnityEngine.Object>)ON_TEXT_CHANGED);
+	}
+
+	private void Start()
+	{
+		StartCoroutine(AnimateVertexColors());
+	}
+
+	private void ON_TEXT_CHANGED(UnityEngine.Object obj)
+	{
+		if (obj == m_TextComponent)
 		{
-			public float angleRange;
-
-			public float angle;
-
-			public float speed;
+			hasTextChanged = true;
 		}
+	}
 
-		[CompilerGenerated]
-		private sealed class _003CAnimateVertexColors_003Ed__11 : IEnumerator<object>, IEnumerator, IDisposable
+	private IEnumerator AnimateVertexColors()
+	{
+		m_TextComponent.ForceMeshUpdate();
+		TMP_TextInfo textInfo = m_TextComponent.textInfo;
+		int loopCount = 0;
+		hasTextChanged = true;
+		VertexAnim[] vertexAnim = new VertexAnim[1024];
+		for (int i = 0; i < 1024; i++)
 		{
-			private int _003C_003E1__state;
-
-			private object _003C_003E2__current;
-
-			public VertexJitter _003C_003E4__this;
-
-			private TMP_TextInfo _003CtextInfo_003E5__2;
-
-			private int _003CloopCount_003E5__3;
-
-			private VertexAnim[] _003CvertexAnim_003E5__4;
-
-			private TMP_MeshInfo[] _003CcachedMeshInfo_003E5__5;
-
-			private object System_002ECollections_002EGeneric_002EIEnumerator_003CSystem_002EObject_003E_002ECurrent
+			vertexAnim[i].angleRange = Random.Range(10f, 25f);
+			vertexAnim[i].speed = Random.Range(1f, 3f);
+		}
+		TMP_MeshInfo[] cachedMeshInfo = textInfo.CopyMeshInfoVertexData();
+		while (true)
+		{
+			if (hasTextChanged)
 			{
-				[DebuggerHidden]
-				get
+				cachedMeshInfo = textInfo.CopyMeshInfoVertexData();
+				hasTextChanged = false;
+			}
+			int characterCount = textInfo.characterCount;
+			if (characterCount == 0)
+			{
+				yield return new WaitForSeconds(0.25f);
+				continue;
+			}
+			for (int j = 0; j < characterCount; j++)
+			{
+				if (textInfo.characterInfo[j].isVisible)
 				{
-					return null;
+					VertexAnim vertexAnim2 = vertexAnim[j];
+					int materialReferenceIndex = textInfo.characterInfo[j].materialReferenceIndex;
+					int vertexIndex = textInfo.characterInfo[j].vertexIndex;
+					Vector3[] vertices = cachedMeshInfo[materialReferenceIndex].vertices;
+					Vector3 vector = (Vector2)((vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2f);
+					Vector3[] vertices2 = textInfo.meshInfo[materialReferenceIndex].vertices;
+					vertices2[vertexIndex] = vertices[vertexIndex] - vector;
+					vertices2[vertexIndex + 1] = vertices[vertexIndex + 1] - vector;
+					vertices2[vertexIndex + 2] = vertices[vertexIndex + 2] - vector;
+					vertices2[vertexIndex + 3] = vertices[vertexIndex + 3] - vector;
+					vertexAnim2.angle = Mathf.SmoothStep(0f - vertexAnim2.angleRange, vertexAnim2.angleRange, Mathf.PingPong((float)loopCount / 25f * vertexAnim2.speed, 1f));
+					Matrix4x4 matrix4x = Matrix4x4.TRS(new Vector3(Random.Range(-0.25f, 0.25f), Random.Range(-0.25f, 0.25f), 0f) * CurveScale, Quaternion.Euler(0f, 0f, Random.Range(-5f, 5f) * AngleMultiplier), Vector3.one);
+					vertices2[vertexIndex] = matrix4x.MultiplyPoint3x4(vertices2[vertexIndex]);
+					vertices2[vertexIndex + 1] = matrix4x.MultiplyPoint3x4(vertices2[vertexIndex + 1]);
+					vertices2[vertexIndex + 2] = matrix4x.MultiplyPoint3x4(vertices2[vertexIndex + 2]);
+					vertices2[vertexIndex + 3] = matrix4x.MultiplyPoint3x4(vertices2[vertexIndex + 3]);
+					vertices2[vertexIndex] += vector;
+					vertices2[vertexIndex + 1] += vector;
+					vertices2[vertexIndex + 2] += vector;
+					vertices2[vertexIndex + 3] += vector;
+					vertexAnim[j] = vertexAnim2;
 				}
 			}
-
-			private object System_002ECollections_002EIEnumerator_002ECurrent
+			for (int k = 0; k < textInfo.meshInfo.Length; k++)
 			{
-				[DebuggerHidden]
-				get
-				{
-					return null;
-				}
+				textInfo.meshInfo[k].mesh.vertices = textInfo.meshInfo[k].vertices;
+				m_TextComponent.UpdateGeometry(textInfo.meshInfo[k].mesh, k);
 			}
-
-			[DebuggerHidden]
-			public _003CAnimateVertexColors_003Ed__11(int _003C_003E1__state)
-			{
-			}
-
-			[DebuggerHidden]
-			private void System_002EIDisposable_002EDispose()
-			{
-			}
-
-			private bool MoveNext()
-			{
-				return false;
-			}
-
-			[DebuggerHidden]
-			private void System_002ECollections_002EIEnumerator_002EReset()
-			{
-			}
-		}
-
-		public float AngleMultiplier;
-
-		public float SpeedMultiplier;
-
-		public float CurveScale;
-
-		private TMP_Text m_TextComponent;
-
-		private bool hasTextChanged;
-
-		private void Awake()
-		{
-		}
-
-		private void OnEnable()
-		{
-		}
-
-		private void OnDisable()
-		{
-		}
-
-		private void Start()
-		{
-		}
-
-		private void ON_TEXT_CHANGED(UnityEngine.Object obj)
-		{
-		}
-
-		[IteratorStateMachine(typeof(_003CAnimateVertexColors_003Ed__11))]
-		private IEnumerator AnimateVertexColors()
-		{
-			return null;
+			loopCount++;
+			yield return new WaitForSeconds(0.1f);
 		}
 	}
 }
