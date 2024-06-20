ce397f06dcdf61e5aacbbff186812e09aa6bf8fd
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:21:24 +0300

    SkewTextExample.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/SkewTextExample.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/SkewTextExample.cs
@@ -1,93 +1,103 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class SkewTextExample : MonoBehaviour
 {
-	public class SkewTextExample : MonoBehaviour
+	private TMP_Text m_TextComponent;
+
+	public AnimationCurve VertexCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.25f, 2f), new Keyframe(0.5f, 0f), new Keyframe(0.75f, 2f), new Keyframe(1f, 0f));
+
+	public float CurveScale = 1f;
+
+	public float ShearAmount = 1f;
+
+	private void Awake()
 	{
-		[CompilerGenerated]
-		private sealed class _003CWarpText_003Ed__7 : IEnumerator<object>, IEnumerator, IDisposable
+		m_TextComponent = base.gameObject.GetComponent<TMP_Text>();
+	}
+
+	private void Start()
+	{
+		StartCoroutine(WarpText());
+	}
+
+	private AnimationCurve CopyAnimationCurve(AnimationCurve curve)
+	{
+		return new AnimationCurve
 		{
-			private int _003C_003E1__state;
+			keys = curve.keys
+		};
+	}
 
-			private object _003C_003E2__current;
-
-			public SkewTextExample _003C_003E4__this;
-
-			private float _003Cold_CurveScale_003E5__2;
-
-			private float _003Cold_ShearValue_003E5__3;
-
-			private AnimationCurve _003Cold_curve_003E5__4;
-
-			private object System_002ECollections_002EGeneric_002EIEnumerator_003CSystem_002EObject_003E_002ECurrent
+	private IEnumerator WarpText()
+	{
+		VertexCurve.preWrapMode = (WrapMode)1;
+		VertexCurve.postWrapMode = (WrapMode)1;
+		m_TextComponent.havePropertiesChanged = true;
+		CurveScale *= 10f;
+		float old_CurveScale = CurveScale;
+		float old_ShearValue = ShearAmount;
+		AnimationCurve old_curve = CopyAnimationCurve(VertexCurve);
+		while (true)
+		{
+			if (!m_TextComponent.havePropertiesChanged && old_CurveScale == CurveScale && old_curve.keys[1].value == VertexCurve.keys[1].value && old_ShearValue == ShearAmount)
 			{
-				[DebuggerHidden]
-				get
+				yield return null;
+				continue;
+			}
+			old_CurveScale = CurveScale;
+			old_curve = CopyAnimationCurve(VertexCurve);
+			old_ShearValue = ShearAmount;
+			m_TextComponent.ForceMeshUpdate();
+			TMP_TextInfo textInfo = m_TextComponent.textInfo;
+			int characterCount = textInfo.characterCount;
+			if (characterCount == 0)
+			{
+				continue;
+			}
+			float x = m_TextComponent.bounds.min.x;
+			float x2 = m_TextComponent.bounds.max.x;
+			for (int i = 0; i < characterCount; i++)
+			{
+				if (textInfo.characterInfo[i].isVisible)
 				{
-					return null;
+					int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+					int materialReferenceIndex = textInfo.characterInfo[i].materialReferenceIndex;
+					Vector3[] vertices = textInfo.meshInfo[materialReferenceIndex].vertices;
+					Vector3 vector = new Vector2((vertices[vertexIndex].x + vertices[vertexIndex + 2].x) / 2f, textInfo.characterInfo[i].baseLine);
+					vertices[vertexIndex] += -vector;
+					vertices[vertexIndex + 1] += -vector;
+					vertices[vertexIndex + 2] += -vector;
+					vertices[vertexIndex + 3] += -vector;
+					float num = ShearAmount * 0.01f;
+					Vector3 vector2 = new Vector3(num * (textInfo.characterInfo[i].topRight.y - textInfo.characterInfo[i].baseLine), 0f, 0f);
+					Vector3 vector3 = new Vector3(num * (textInfo.characterInfo[i].baseLine - textInfo.characterInfo[i].bottomRight.y), 0f, 0f);
+					vertices[vertexIndex] += -vector3;
+					vertices[vertexIndex + 1] += vector2;
+					vertices[vertexIndex + 2] += vector2;
+					vertices[vertexIndex + 3] += -vector3;
+					float num2 = (vector.x - x) / (x2 - x);
+					float num3 = num2 + 0.0001f;
+					float y = VertexCurve.Evaluate(num2) * CurveScale;
+					float y2 = VertexCurve.Evaluate(num3) * CurveScale;
+					Vector3 lhs = new Vector3(1f, 0f, 0f);
+					Vector3 rhs = new Vector3(num3 * (x2 - x) + x, y2) - new Vector3(vector.x, y);
+					float num4 = Mathf.Acos(Vector3.Dot(lhs, rhs.normalized)) * 57.29578f;
+					float z = ((Vector3.Cross(lhs, rhs).z > 0f) ? num4 : (360f - num4));
+					Matrix4x4 matrix4x = Matrix4x4.TRS(new Vector3(0f, y, 0f), Quaternion.Euler(0f, 0f, z), Vector3.one);
+					vertices[vertexIndex] = matrix4x.MultiplyPoint3x4(vertices[vertexIndex]);
+					vertices[vertexIndex + 1] = matrix4x.MultiplyPoint3x4(vertices[vertexIndex + 1]);
+					vertices[vertexIndex + 2] = matrix4x.MultiplyPoint3x4(vertices[vertexIndex + 2]);
+					vertices[vertexIndex + 3] = matrix4x.MultiplyPoint3x4(vertices[vertexIndex + 3]);
+					vertices[vertexIndex] += vector;
+					vertices[vertexIndex + 1] += vector;
+					vertices[vertexIndex + 2] += vector;
+					vertices[vertexIndex + 3] += vector;
 				}
 			}
-
-			private object System_002ECollections_002EIEnumerator_002ECurrent
-			{
-				[DebuggerHidden]
-				get
-				{
-					return null;
-				}
-			}
-
-			[DebuggerHidden]
-			public _003CWarpText_003Ed__7(int _003C_003E1__state)
-			{
-			}
-
-			[DebuggerHidden]
-			private void System_002EIDisposable_002EDispose()
-			{
-			}
-
-			private bool MoveNext()
-			{
-				return false;
-			}
-
-			[DebuggerHidden]
-			private void System_002ECollections_002EIEnumerator_002EReset()
-			{
-			}
-		}
-
-		private TMP_Text m_TextComponent;
-
-		public AnimationCurve VertexCurve;
-
-		public float CurveScale;
-
-		public float ShearAmount;
-
-		private void Awake()
-		{
-		}
-
-		private void Start()
-		{
-		}
-
-		private AnimationCurve CopyAnimationCurve(AnimationCurve curve)
-		{
-			return null;
-		}
-
-		[IteratorStateMachine(typeof(_003CWarpText_003Ed__7))]
-		private IEnumerator WarpText()
-		{
-			return null;
+			m_TextComponent.UpdateVertexData();
+			yield return null;
 		}
 	}
 }
