6a3fc7934ecb923dc811744de80840776f9aca25
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:29:35 +0300

    VertexColorCycler.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/VertexColorCycler.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/VertexColorCycler.cs
@@ -1,80 +1,45 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class VertexColorCycler : MonoBehaviour
 {
-	public class VertexColorCycler : MonoBehaviour
+	private TMP_Text m_TextComponent;
+
+	private void Awake()
 	{
-		[CompilerGenerated]
-		private sealed class _003CAnimateVertexColors_003Ed__3 : IEnumerator<object>, IEnumerator, IDisposable
+		m_TextComponent = GetComponent<TMP_Text>();
+	}
+
+	private void Start()
+	{
+		StartCoroutine(AnimateVertexColors());
+	}
+
+	private IEnumerator AnimateVertexColors()
+	{
+		m_TextComponent.ForceMeshUpdate();
+		TMP_TextInfo textInfo = m_TextComponent.textInfo;
+		int currentCharacter = 0;
+		_ = (Color32)m_TextComponent.color;
+		while (true)
 		{
-			private int _003C_003E1__state;
-
-			private object _003C_003E2__current;
-
-			public VertexColorCycler _003C_003E4__this;
-
-			private TMP_TextInfo _003CtextInfo_003E5__2;
-
-			private int _003CcurrentCharacter_003E5__3;
-
-			private object System_002ECollections_002EGeneric_002EIEnumerator_003CSystem_002EObject_003E_002ECurrent
+			int characterCount = textInfo.characterCount;
+			if (characterCount == 0)
 			{
-				[DebuggerHidden]
-				get
-				{
-					return null;
-				}
+				yield return new WaitForSeconds(0.25f);
+				continue;
 			}
-
-			private object System_002ECollections_002EIEnumerator_002ECurrent
+			int materialReferenceIndex = textInfo.characterInfo[currentCharacter].materialReferenceIndex;
+			Color32[] colors = textInfo.meshInfo[materialReferenceIndex].colors32;
+			int vertexIndex = textInfo.characterInfo[currentCharacter].vertexIndex;
+			if (textInfo.characterInfo[currentCharacter].isVisible)
 			{
-				[DebuggerHidden]
-				get
-				{
-					return null;
-				}
+				colors[vertexIndex + 3] = (colors[vertexIndex + 2] = (colors[vertexIndex + 1] = (colors[vertexIndex] = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), byte.MaxValue))));
+				m_TextComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
 			}
-
-			[DebuggerHidden]
-			public _003CAnimateVertexColors_003Ed__3(int _003C_003E1__state)
-			{
-			}
-
-			[DebuggerHidden]
-			private void System_002EIDisposable_002EDispose()
-			{
-			}
-
-			private bool MoveNext()
-			{
-				return false;
-			}
-
-			[DebuggerHidden]
-			private void System_002ECollections_002EIEnumerator_002EReset()
-			{
-			}
-		}
-
-		private TMP_Text m_TextComponent;
-
-		private void Awake()
-		{
-		}
-
-		private void Start()
-		{
-		}
-
-		[IteratorStateMachine(typeof(_003CAnimateVertexColors_003Ed__3))]
-		private IEnumerator AnimateVertexColors()
-		{
-			return null;
+			currentCharacter = (currentCharacter + 1) % characterCount;
+			yield return new WaitForSeconds(0.05f);
 		}
 	}
 }
