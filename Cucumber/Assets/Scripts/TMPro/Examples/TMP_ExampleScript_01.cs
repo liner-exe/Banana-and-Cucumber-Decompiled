36a772de9875b7d0a834c4f14e74a9c8e0365a62
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:24:43 +0300

    TMP_ExampleScript_01.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/TMP_ExampleScript_01.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/TMP_ExampleScript_01.cs
@@ -1,31 +1,49 @@
+using TMPro;
+using TMPro.Examples;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class TMP_ExampleScript_01 : MonoBehaviour
 {
-	public class TMP_ExampleScript_01 : MonoBehaviour
+	public enum objectType
 	{
-		public enum objectType
+		TextMeshPro,
+		TextMeshProUGUI
+	}
+
+	public objectType ObjectType;
+
+	public bool isStatic;
+
+	private TMP_Text m_text;
+
+	private const string k_label = "The count is <#0080ff>{0}</color>";
+
+	private int count;
+
+	private void Awake()
+	{
+		if (ObjectType == objectType.TextMeshPro)
 		{
-			TextMeshPro = 0,
-			TextMeshProUGUI = 1
+			m_text = GetComponent<TextMeshPro>() ?? base.gameObject.AddComponent<TextMeshPro>();
 		}
-
-		public objectType ObjectType;
-
-		public bool isStatic;
-
-		private TMP_Text m_text;
-
-		private const string k_label = "The count is <#0080ff>{0}</color>";
-
-		private int count;
-
-		private void Awake()
+		else
 		{
+			m_text = GetComponent<TextMeshProUGUI>() ?? base.gameObject.AddComponent<TextMeshProUGUI>();
 		}
+		m_text.font = Resources.Load<TMP_FontAsset>("Fonts & Materials/Anton SDF");
+		m_text.fontSharedMaterial = Resources.Load<Material>("Fonts & Materials/Anton SDF - Drop Shadow");
+		m_text.fontSize = 120f;
+		m_text.text = "A <#0080ff>simple</color> line of text.";
+		Vector2 preferredValues = m_text.GetPreferredValues(float.PositiveInfinity, float.PositiveInfinity);
+		m_text.rectTransform.sizeDelta = new Vector2(preferredValues.x, preferredValues.y);
+	}
 
-		private void Update()
+	private void Update()
+	{
+		if (!isStatic)
 		{
+			m_text.SetText("The count is <#0080ff>{0}</color>", count % 1000);
+			count++;
 		}
 	}
 }
