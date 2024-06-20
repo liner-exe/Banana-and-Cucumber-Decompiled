870ac731c889f2cc99241c7e297f2ead87d133ce
liner.exe <90984207+liner-exe@users.noreply.github.com>
2024-06-20 23:31:09 +0300

    VertexShakeB.cs

--- Cucumber/Assets/Scripts/TMPro/Examples/VertexShakeB.cs
+++ Cucumber/Assets/Scripts/TMPro/Examples/VertexShakeB.cs
@@ -1,100 +1,125 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
-namespace TMPro.Examples
+public class VertexShakeB : MonoBehaviour
 {
-	public class VertexShakeB : MonoBehaviour
+	public float AngleMultiplier = 1f;
+
+	public float SpeedMultiplier = 1f;
+
+	public float CurveScale = 1f;
+
+	private TMP_Text m_TextComponent;
+
+	private bool hasTextChanged;
+
+	private void Awake()
 	{
-		[CompilerGenerated]
-		private sealed class _003CAnimateVertexColors_003Ed__10 : IEnumerator<object>, IEnumerator, IDisposable
+		m_TextComponent = GetComponent<TMP_Text>();
+	}
+
+	private void OnEnable()
+	{
+		TMPro_EventManager.TEXT_CHANGED_EVENT.Add((Action<UnityEngine.Object>)ON_TEXT_CHANGED);
+	}
+
+	private void OnDisable()
+	{
+		TMPro_EventManager.TEXT_CHANGED_EVENT.Remove((Action<UnityEngine.Object>)ON_TEXT_CHANGED);
+	}
+
+	private void Start()
+	{
+		StartCoroutine(AnimateVertexColors());
+	}
+
+	private void ON_TEXT_CHANGED(UnityEngine.Object obj)
+	{
+		if ((bool)(obj = m_TextComponent))
 		{
-			private int _003C_003E1__state;
+			hasTextChanged = true;
+		}
+	}
 
-			private object _003C_003E2__current;
-
-			public VertexShakeB _003C_003E4__this;
-
-			private TMP_TextInfo _003CtextInfo_003E5__2;
-
-			private Vector3[][] _003CcopyOfVertices_003E5__3;
-
-			private object System_002ECollections_002EGeneric_002EIEnumerator_003CSystem_002EObject_003E_002ECurrent
+	private IEnumerator AnimateVertexColors()
+	{
+		m_TextComponent.ForceMeshUpdate();
+		TMP_TextInfo textInfo = m_TextComponent.textInfo;
+		Vector3[][] copyOfVertices = new Vector3[0][];
+		hasTextChanged = true;
+		while (true)
+		{
+			if (hasTextChanged)
 			{
-				[DebuggerHidden]
-				get
+				if (copyOfVertices.Length < textInfo.meshInfo.Length)
 				{
-					return null;
+					copyOfVertices = new Vector3[textInfo.meshInfo.Length][];
+				}
+				for (int i = 0; i < textInfo.meshInfo.Length; i++)
+				{
+					int num = textInfo.meshInfo[i].vertices.Length;
+					copyOfVertices[i] = new Vector3[num];
+				}
+				hasTextChanged = false;
+			}
+			if (textInfo.characterCount == 0)
+			{
+				yield return new WaitForSeconds(0.25f);
+				continue;
+			}
+			int lineCount = textInfo.lineCount;
+			for (int j = 0; j < lineCount; j++)
+			{
+				int firstCharacterIndex = textInfo.lineInfo[j].firstCharacterIndex;
+				int lastCharacterIndex = textInfo.lineInfo[j].lastCharacterIndex;
+				Vector3 vector = (textInfo.characterInfo[firstCharacterIndex].bottomLeft + textInfo.characterInfo[lastCharacterIndex].topRight) / 2f;
+				Quaternion q = Quaternion.Euler(0f, 0f, Random.Range(-0.25f, 0.25f));
+				for (int k = firstCharacterIndex; k <= lastCharacterIndex; k++)
+				{
+					if (textInfo.characterInfo[k].isVisible)
+					{
+						int materialReferenceIndex = textInfo.characterInfo[k].materialReferenceIndex;
+						int vertexIndex = textInfo.characterInfo[k].vertexIndex;
+						Vector3[] vertices = textInfo.meshInfo[materialReferenceIndex].vertices;
+						Vector3 vector2 = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2f;
+						copyOfVertices[materialReferenceIndex][vertexIndex] = vertices[vertexIndex] - vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 1] = vertices[vertexIndex + 1] - vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 2] = vertices[vertexIndex + 2] - vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 3] = vertices[vertexIndex + 3] - vector2;
+						float num2 = Random.Range(0.95f, 1.05f);
+						Matrix4x4 matrix4x = Matrix4x4.TRS(Vector3.one, Quaternion.identity, Vector3.one * num2);
+						copyOfVertices[materialReferenceIndex][vertexIndex] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex]);
+						copyOfVertices[materialReferenceIndex][vertexIndex + 1] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex + 1]);
+						copyOfVertices[materialReferenceIndex][vertexIndex + 2] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex + 2]);
+						copyOfVertices[materialReferenceIndex][vertexIndex + 3] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex + 3]);
+						copyOfVertices[materialReferenceIndex][vertexIndex] += vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 1] += vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 2] += vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 3] += vector2;
+						copyOfVertices[materialReferenceIndex][vertexIndex] -= vector;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 1] -= vector;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 2] -= vector;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 3] -= vector;
+						matrix4x = Matrix4x4.TRS(Vector3.one, q, Vector3.one);
+						copyOfVertices[materialReferenceIndex][vertexIndex] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex]);
+						copyOfVertices[materialReferenceIndex][vertexIndex + 1] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex + 1]);
+						copyOfVertices[materialReferenceIndex][vertexIndex + 2] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex + 2]);
+						copyOfVertices[materialReferenceIndex][vertexIndex + 3] = matrix4x.MultiplyPoint3x4(copyOfVertices[materialReferenceIndex][vertexIndex + 3]);
+						copyOfVertices[materialReferenceIndex][vertexIndex] += vector;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 1] += vector;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 2] += vector;
+						copyOfVertices[materialReferenceIndex][vertexIndex + 3] += vector;
+					}
 				}
 			}
-
-			private object System_002ECollections_002EIEnumerator_002ECurrent
+			for (int l = 0; l < textInfo.meshInfo.Length; l++)
 			{
-				[DebuggerHidden]
-				get
-				{
-					return null;
-				}
+				textInfo.meshInfo[l].mesh.vertices = copyOfVertices[l];
+				m_TextComponent.UpdateGeometry(textInfo.meshInfo[l].mesh, l);
 			}
-
-			[DebuggerHidden]
-			public _003CAnimateVertexColors_003Ed__10(int _003C_003E1__state)
-			{
-			}
-
-			[DebuggerHidden]
-			private void System_002EIDisposable_002EDispose()
-			{
-			}
-
-			private bool MoveNext()
-			{
-				return false;
-			}
-
-			[DebuggerHidden]
-			private void System_002ECollections_002EIEnumerator_002EReset()
-			{
-			}
-		}
-
-		public float AngleMultiplier;
-
-		public float SpeedMultiplier;
-
-		public float CurveScale;
-
-		private TMP_Text m_TextComponent;
-
-		private bool hasTextChanged;
-
-		private void Awake()
-		{
-		}
-
-		private void OnEnable()
-		{
-		}
-
-		private void OnDisable()
-		{
-		}
-
-		private void Start()
-		{
-		}
-
-		private void ON_TEXT_CHANGED(UnityEngine.Object obj)
-		{
-		}
-
-		[IteratorStateMachine(typeof(_003CAnimateVertexColors_003Ed__10))]
-		private IEnumerator AnimateVertexColors()
-		{
-			return null;
+			yield return new WaitForSeconds(0.1f);
 		}
 	}
 }
